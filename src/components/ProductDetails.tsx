0261325ba1db3d9aa69ffcebcc6984dc01202c05
M.Atif <shahzaibengr2010@gmail.com>
2025-06-06 16:02:53 +0100

    update ProductDetails.tsx

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,5 +1,5 @@
 // components/ProductDetails.tsx
-'use client';
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -15,19 +15,19 @@ export function ProductDetails({
   const [imgLoaded, setImgLoaded] = useState(false);
 
   return (
-    <main className="max-w-xl mx-auto p-6 rounded-lg shadow-md border bg-white mt-10">
+    <main className="max-w-xl mx-auto p-6 rounded-lg shadow-md border bg-white mt-10 flex flex-col items-center">
       <h1 className="text-3xl font-bold mb-4 text-gray-900">{name}</h1>
-      <p className="text-lg text-gray-700">{description}</p>
-      <div className="relative w-[200px] h-[200px]">
+      <p className="text-lg mb-4 text-gray-700">{description}</p>
+      <div className="relative w-[300px] h-[300px] flex justify-center items-center">
         {!imgLoaded && (
-          <div className="absolute inset-0 animate-pulse bg-gray-200 rounded" />
+          <div className="absolute inset-0 animate-pulse bg-gray-300 rounded" />
         )}
         {imageUrl && (
           <Image
             src={imageUrl}
             alt="Product"
-            width={200}
-            height={200}
+            width={300}
+            height={300}
             placeholder="empty"
             style={{ objectFit: "contain" }}
             onLoad={() => setImgLoaded(true)}
