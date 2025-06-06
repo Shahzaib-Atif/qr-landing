6bf8877c64b0f9630f971a4b48f7b4d9071906ab
M.Atif <shahzaibengr2010@gmail.com>
2025-06-06 16:48:31 +0100

    minor edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { SupabaseProductRepository } from "@/lib/repositories/products/supabase-product-repository";
-import Image from "next/image";
+// import Image from "next/image";
 
 export default async function Page() {
   const repo = new SupabaseProductRepository();
