8c1f64eea79970e8c3f9ab894e54734a26c01257
M.Atif <shahzaibengr2010@gmail.com>
2025-06-06 15:51:31 +0100

    show image more efficiently

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**", // This matches any hostname (use with caution)
+      },
+    ],
+  }
 };
 
 export default nextConfig;
--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,4 +1,8 @@
 // components/ProductDetails.tsx
+'use client';
+import Image from "next/image";
+import { useState } from "react";
+
 export function ProductDetails({
   name,
   description,
@@ -8,16 +12,31 @@ export function ProductDetails({
   description: string | null;
   imageUrl: string | null;
 }) {
+  const [imgLoaded, setImgLoaded] = useState(false);
+
   return (
     <main className="max-w-xl mx-auto p-6 rounded-lg shadow-md border bg-white mt-10">
       <h1 className="text-3xl font-bold mb-4 text-gray-900">{name}</h1>
       <p className="text-lg text-gray-700">{description}</p>
-      {imageUrl ? (
-        // eslint-disable-next-line @next/next/no-img-element
-        <img src={imageUrl} alt="Private" width={200} height={"auto"} />
-      ) : (
-        <p>Loading image...</p>
-      )}
+      <div className="relative w-[200px] h-[200px]">
+        {!imgLoaded && (
+          <div className="absolute inset-0 animate-pulse bg-gray-200 rounded" />
+        )}
+        {imageUrl && (
+          <Image
+            src={imageUrl}
+            alt="Product"
+            width={200}
+            height={200}
+            placeholder="empty"
+            style={{ objectFit: "contain" }}
+            onLoad={() => setImgLoaded(true)}
+            className={`transition-opacity duration-300 ${
+              imgLoaded ? "opacity-100" : "opacity-0"
+            }`}
+          />
+        )}
+      </div>
     </main>
   );
 }
